4382d857fd020aad7e2b573d52186a1439785926
Sh4y0 <Sh4y0>
2025-04-05 00:31:46 -0500

    Inicializar el proyecto ITN LMS con Java + Springboot

---
+++ src/main/java/com/shayo/platformapp/itn_learning_platform/ItnLearningPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.shayo.platformapp.itn_learning_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItnLearningPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItnLearningPlatformApplication.class, args);
+    }
+
+}
